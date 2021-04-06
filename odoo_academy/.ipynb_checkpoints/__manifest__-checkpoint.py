48d3a0a60ed57fa8ab609fd1c85eb5ebf21747bf
criniguez-hexacorp <cristian.iniguez@hexacorp.com.bo>
2021-04-06 13:59:29 +0000

    [ADD] Basic Module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    
+    'author': 'Cristian Iñiguez',
+    
+    'website': 'https://www.cristianiniguez.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [],
+    
+    'demo': [],
+}
