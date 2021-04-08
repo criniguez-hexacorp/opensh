3fd3b134486ad1bf7af723aeb67691983798b43b
criniguez-hexacorp <cristian.iniguez@hexacorp.com.bo>
2021-04-08 14:02:13 +0000

    [ADD] Views

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -21,7 +21,12 @@
     
     'depends': ['base'],
     
-    'data': [],
+    'data': [
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
+        'views/academy_course_views.xml'
+    ],
     
     'demo': [
         'demo/academy_course_demo.xml',
